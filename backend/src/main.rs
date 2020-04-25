b998f2aef3b6ff9925ce034e283537cbc22015f4
BrainVader <brainvade@gmail.com>
2020-04-25 19:41:31 +0900

    Add server template

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,14 @@
+use actix_web::{App, HttpServer};
+
 #[actix_rt::main]
-async fn main() {
-    println!("Hello Backend");
+async fn main() -> std::io::Result<()> {
+    let localhost = std::net::Ipv4Addr::new(127, 0, 0, 1);
+    let ip = std::net::IpAddr::V4(localhost);
+    let port = 8080;
+    let addr = std::net::SocketAddr::new(ip, port);
+
+    let app_factory = || App::new();
+
+    let server = HttpServer::new(app_factory);
+    server.bind(addr)?.run().await
 }
