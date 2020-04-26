a8bf475127fc351b6e912eb277631ed7d296bb8f
BrainVader <brainvade@gmail.com>
2020-04-26 22:42:31 +0900

    Add graphiql endpoint

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,13 @@
-use actix_web::{App, HttpServer};
+use actix_web::{get, App, HttpResponse, HttpServer};
+use juniper::http::graphiql::graphiql_source;
+
+#[get("/graphiql")]
+async fn graphiql() -> HttpResponse {
+    let html = graphiql_source("http://127.0.0.1:8080/graphql");
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -7,7 +16,7 @@ async fn main() -> std::io::Result<()> {
     let port = 8080;
     let addr = std::net::SocketAddr::new(ip, port);
 
-    let app_factory = || App::new();
+    let app_factory = || App::new().service(graphiql);
 
     let server = HttpServer::new(app_factory);
     server.bind(addr)?.run().await
