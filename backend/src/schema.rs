2fb117a6162a949216778ca00c8016833ca9548a
BrainVader <brainvade@gmail.com>
2020-04-26 23:18:40 +0900

    Add Query type

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -1,3 +1,5 @@
+use juniper::FieldResult;
+
 use juniper::ID;
 
 enum Episode {
@@ -22,4 +24,17 @@ struct Evangelion {
     id: ID,
     name: String,
     appears_in: Vec<Episode>,
+}
+
+pub struct QueryRoot;
+
+#[juniper::object]
+impl QueryRoot {
+    fn human(id: String) -> FieldResult<Human> {
+        Ok(Human {
+            id: ID::from("1".to_owned()),
+            name: "Shinji Ikari".to_owned(),
+            appears_in: vec![Episode::Jo, Episode::Ha, Episode::Q],
+        })
+    }
 }
