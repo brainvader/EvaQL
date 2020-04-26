541480e267a5b86ad846e8f00f302e77a8662454
BrainVader <brainvade@gmail.com>
2020-04-26 23:20:56 +0900

    Add macros

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -1,25 +1,29 @@
 use juniper::FieldResult;
-
 use juniper::ID;
+use juniper::{GraphQLEnum, GraphQLInputObject, GraphQLObject};
 
+#[derive(GraphQLEnum)]
 enum Episode {
     Jo,
     Ha,
     Q,
 }
 
+#[derive(GraphQLObject)]
 struct Human {
     id: ID,
     name: String,
     appears_in: Vec<Episode>,
 }
 
+#[derive(GraphQLObject)]
 struct Angel {
     id: ID,
     name: String,
     appears_in: Vec<Episode>,
 }
 
+#[derive(GraphQLObject)]
 struct Evangelion {
     id: ID,
     name: String,
