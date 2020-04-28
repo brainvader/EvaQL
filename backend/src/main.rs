6ee350f75b1d44d434e6466cf4d4bc837d8851d8
BrainVader <brainvade@gmail.com>
2020-04-28 22:17:01 +0900

    Add graphql_endpoints configuration

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -31,6 +31,14 @@ async fn graphql(
     Ok(response)
 }
 
+// Each ServiceConfig can have it's own data, routes, and services.
+// see https://actix.rs/docs/application/
+// taken from https://github.com/lucperkins/rust-graphql-juniper-actix-diesel-postgres/blob/015cf2e116124f8553ee31263ff29ecc8a1bfa3f/src/endpoints.rs
+fn graphql_endpoints(config: &mut web::ServiceConfig) {
+    let schema = std::sync::Arc::new(create_schema());
+    config.data(schema).service(graphql).service(graphiql);
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let localhost = std::net::Ipv4Addr::new(127, 0, 0, 1);
@@ -38,14 +46,7 @@ async fn main() -> std::io::Result<()> {
     let port = 8080;
     let addr = std::net::SocketAddr::new(ip, port);
 
-    let schema = std::sync::Arc::new(create_schema());
-
-    let app_factory = move || {
-        App::new()
-            .data(schema.clone())
-            .service(graphiql)
-            .service(graphql)
-    };
+    let app_factory = move || App::new().configure(graphql_endpoints);
 
     let mut server = HttpServer::new(app_factory);
     let mut listenfd = ListenFd::from_env();
